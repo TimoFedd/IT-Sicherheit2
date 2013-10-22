69381bf4431e73c32550bc32e44bdfadb8f21c21
Timo <timoFedd>
2013-10-22 13:35:01 +0200

    Erster Test commit

---
+++ IT-Sicherheit2/src/LCG.java
@@ -0,0 +1,28 @@
+
+public class LCG {
+
+	int startwert;
+	
+	public int nextValue(){
+		
+		
+		return 0;
+	}
+	
+	
+	
+	public LCG(int startwert){
+		this.startwert=startwert;
+	}
+	
+	
+	public static void main(String[] args) {
+	
+
+		LCG generator = new LCG(5);
+		System.out.println("Hello world :)");
+		
+		
+	}
+
+}

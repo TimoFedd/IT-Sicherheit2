61e7088e3f29b1a4f98b94401e88fdfebea5b9a0
Timo <timoFedd>
2013-10-22 14:15:52 +0200

    Unnötige Variable "startwert" entfernt

--- IT-Sicherheit2/src/LCG.java
+++ IT-Sicherheit2/src/LCG.java
@@ -1,30 +1,27 @@
-
 public class LCG {
 
-	double startwert;
+	
 	double x;
 	double a = 2653;
 	double b = 1;
 	double n = Math.pow(2, 32);
-	
-	   public int nextValue(){
-		
-       double next= (a*x+b)%n;
-	   x = next;
 
-	      if(next<n/2)
-	    	  return 0;
-	      else
-	    	 return 1;
-       }
-	
-	public LCG(int startwert){
-	  	   x=startwert;
-	   }
-	
-	
+	public LCG(int startwert) {
+		x = startwert;
+	}
+
+	public int nextValue() {
+
+		double next = (a * x + b) % n;
+		x = next;
+
+		if (next < n / 2)
+			return 0;
+		else
+			return 1;
+	}
+
 	public static void main(String[] args) {
-	
 
 		LCG generator = new LCG(6);
 		System.out.println(generator.nextValue());
@@ -36,7 +33,6 @@ public class LCG {
 		System.out.println(generator.nextValue());
 		System.out.println(generator.nextValue());
 
-		
 	}
 
 }
